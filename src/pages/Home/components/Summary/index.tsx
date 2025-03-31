3daff5f56ad900aeeed1a13fce1a30ce30e40748
LilVictor39 <victordamata1@gmail.com>
2025-03-31 19:29:06 -0300

    Respostas

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -1,40 +1,67 @@
+import { useEffect, useState } from "react";
+import { api } from "../../../../ilb/api";
 import { SummaryAnchors, SummaryContainer, SummaryHeader } from "./styles";
-
 import { ArrowUpRight, Buildings, GithubLogo, Users } from "phosphor-react";
 
+api.get
+interface UserProps {
+  avatar_url: string;
+  name: string;
+  bio: string;
+  login: string;
+  location: string;
+  followers: number;
+}
 
 export function Summary() {
+  const [user, setUser] = useState<UserProps | null>(null);
+  const [loading, setLoading] = useState(true);
 
-  // "https://api.github.com/users", "/lucaspedronet"
-  // "https://api.github.com/search"
-  // "https://api.github.com/repos/lucaspedronet/TudoLista/issues"
+  useEffect(() => {
+    
+    api.get<UserProps>('/users/LilVictor39')
+      .then(response => {
+        setUser(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Erro ao buscar os dados do GitHub:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading || !user) {
+    return (
+      <SummaryContainer>
+        <p>Aguarde, carregando os dados do GitHub...</p>
+      </SummaryContainer>
+    );
+  }
 
   return (
     <SummaryContainer>
-      <img src="../src/assets/avatar.JPG" />
+      <img src={user.avatar_url} alt={user.name} />
       <section>
         <SummaryHeader>
-          <h1>Lucas Pedro</h1>
-          <a href="#" target="_blank">
+          <h1>{user.name}</h1>
+          <a href={`https://github.com/${user.login}`} target="_blank" rel="noopener noreferrer">
             GITHUB
             <ArrowUpRight size={12} />
           </a>
         </SummaryHeader>
-        <p>Software Engineering. developer at NodeJS, ReactJS, React Native, Electron.</p>
+        <p>{user.bio}</p>
         <SummaryAnchors>
           <div>
             <GithubLogo size={18} />
-            <span>lucaspedronet</span>
+            <span>{user.login}</span>
           </div>
-
           <div>
             <Buildings size={18} />
-            <span>Paraná Banco</span>
+            <span>{user.location}</span>
           </div>
-
           <div>
             <Users size={18} />
-            <span>47</span>
+            <span>{user.followers}</span>
           </div>
         </SummaryAnchors>
       </section>
