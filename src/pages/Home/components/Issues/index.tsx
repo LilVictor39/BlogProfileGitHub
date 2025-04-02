eb4dd91589ad56710286921905b1c4575bef57e0
LilVictor39 <victordamata1@gmail.com>
2025-04-02 20:27:07 -0300

    issues-timeline

--- src/pages/Home/components/Issues/index.tsx
+++ src/pages/Home/components/Issues/index.tsx
@@ -1,15 +1,22 @@
-// import { dateFormatter } from "../../../../utils/formatter";
+import { dateFormatter } from "../../../../utils/formatter";
 import { IssuesContainer, StyledNavLink } from "./styles";
 
-export function Issues() {
+interface IssuesProps {
+  number: number;
+  title: string;
+  body: string;
+  created_at: string;
+}
+
+export function Issues({ number, title, body, created_at }: IssuesProps) {
   return (
     <IssuesContainer>
-      <StyledNavLink to="/issue">
+      <StyledNavLink to={`/post/${number}`}>
         <div>
-          <h2>Exemplo de issue</h2>
-          <span>20/03/2025</span>
+          <h2>{title}</h2>
+          <span>{dateFormatter.format(new Date(created_at))}</span>
         </div>
-        <p>body</p>
+        <p>{body?.length > 200 ? `${body?.slice(0, 200)}...` : body}</p>
       </StyledNavLink>
     </IssuesContainer>
   );
